045fe50af59c6ac5e609f5ea23385f82a71d57ff
Pavel <hawkrammfan@gmail.com>
2021-09-03 12:50:46 +0300

    add component PostItem for 1 el

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import React, {useState} from 'react';
+import { PostItem } from './components/PostItem';
+import './styles/App.css';
 
 function App() {
+
+  const [value, setValue] = useState('текст в инпуте');
+
+
   return (
-        <div className="App" >
+        <div className="App">
+          <PostItem post ={{id: 1, title: 'Javascript', body: 'Description'}} />
         </div>
     );
 }
