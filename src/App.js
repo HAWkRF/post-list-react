453c2e3822ec2c156f09c72252b2a02c88794de9
Pavel <hawkrammfan@gmail.com>
2021-09-03 13:28:36 +0300

    posts state, map, props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React, {useState} from 'react';
-import { PostItem } from './components/PostItem';
-import './styles/App.css';
+import React, { useState } from "react";
+import { PostItem } from "./components/PostItem";
+import "./styles/App.css";
 
 function App() {
-
-  const [value, setValue] = useState('текст в инпуте');
-
+  const [posts, setPosts] = useState([
+    { id: 1, title: "Javascript 1", body: "Description 1" },
+    { id: 2, title: "Javascript 2", body: "Description 2 " },
+    { id: 3, title: "Javascript 3", body: "Description 3" },
+  ]);
 
   return (
-        <div className="App">
-          <PostItem post ={{id: 1, title: 'Javascript', body: 'Description'}} />
-        </div>
-    );
+    <div className="App">
+        <h1>Список постов</h1>
+      {posts.map(post => 
+          <PostItem post={post} key={post.id}/>
+        )}
+    </div>
+  );
 }
 
-    export default App;
+export default App;
