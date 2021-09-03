218ddee571e56f4edaf76a61af58bdb76e090964
Pavel <hawkrammfan@gmail.com>
2021-09-03 15:48:42 +0300

    PostList add

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
 import { PostItem } from "./components/PostItem";
+import { PostList } from "./components/PostList";
 import "./styles/App.css";
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: 1, title: "Javascript 1", body: "Description 1" },
-    { id: 2, title: "Javascript 2", body: "Description 2 " },
-    { id: 3, title: "Javascript 3", body: "Description 3" },
+    { id: 1, title: "Javascript 1", body: "Lorem ipsum dolor sit amet." },
+    { id: 2, title: "Javascript 2", body: "Lorem ipsum dolor sit amet consectetur adipisicing." },
+    { id: 3, title: "Javascript 3", body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, voluptas." },
   ]);
 
   return (
     <div className="App">
-        <h1>Список постов</h1>
-      {posts.map(post => 
-          <PostItem post={post} key={post.id}/>
-        )}
+      <form>
+        <input type="text" placeholder="Заголовок"/>
+        <input type="text" placeholder="Описание"/>
+        <button>Создать пост</button>
+      </form>
+      <PostList posts={posts} title="Список постов" />
     </div>
   );
 }
